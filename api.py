95807509f3e7d2ff31103fc837c5687e65b51594
wanly13 <wanlyobregon13@gmail.com>
2023-06-12 14:14:18 -0500

    prueba 2

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-#from src.search import search_tweet
+from src.search import search_tweet
 app = FastAPI()
 
 @app.get("/prueba")
 def read_root():
     print("read")
-    return "search_tweet(prueba' , 2)"
+    return search_tweet('prueba' , 2)
