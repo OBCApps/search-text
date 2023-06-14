cfa9ccc3e3d8a266a616d0a42f73bead4309541a
wanly13 <marco.obregon@utec.edu.pe>
2023-06-14 00:02:22 -0500

    rnt

--- src/search.py
+++ src/search.py
@@ -66,8 +66,9 @@ def documentos_relevantes(query):
     
     for i in tf:
         
-        
-         
+        print("tf[i]" , i)
+        print("names_docs" , nanmes_docs)
+        print("inverted[i]" , inverted[i])
         wtfidf = math.log(1 + tf[i]) * math.log(len(nanmes_docs) / len(inverted[i].split(';'))    ) 
         
         
