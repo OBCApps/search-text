8284826b5ff36f60f309d3d886991ee40e45b09e
wanly13 <wanlyobregon13@gmail.com>
2023-06-17 18:26:51 -0500

    arreglos

--- src/clean.py
+++ src/clean.py
@@ -57,46 +57,6 @@ def remove_emojis(data):
                       "]+", re.UNICODE)
     return re.sub(emoj, '', data)
 
-""" def strip_emoji(text):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u200b"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
-                               u"\u2066"
-                               u"\u2069"
-                               u"\u0144"
-                               u"\u0148"
-                               u"\u2192"
-                               u"\u2105"
-                               u"\u02dd"
-                               u"\u0123"
-                               u"\u0111"
-                               u"\u013a"
-                               u"\u2193"
-                               u"\u2191"
-                               u"\u0307"
-                               u"\u0435"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text)
-
- """
-
 def clean_all(text):
     ans = []
     palabras = nltk.word_tokenize(remove_URL(remove_signes(remove_emojis(text))).lower())
--- src/clean_tweets.py
+++ src/clean_tweets.py
@@ -2,8 +2,8 @@
 import os
 import json
 
-dataset_input = "./src/dataset/data_elecciones"
-#dataset_input = "dataset\\data_elecciones_dev"
+#dataset_input = "./src/dataset/data_elecciones"
+dataset_input = "dataset\\data_elecciones_dev"
 diretion_new_dataset = "clean_data"
 
 def load_file(filename):
@@ -12,23 +12,37 @@ def load_file(filename):
         return json.load(file)
 
 def id_text(tweets):
+    
     clean_tweets = {}
     for tweet in tweets:
         if "id" in tweet and "text" in tweet:
             clean_tweets[tweet["id"]] = tweet["text"]
     return clean_tweets
 
+# def write_file(clean_tweets, filename):
+#     print(f"clean-Write File : {filename}")
+#     directory = os.path.dirname(filename)
+    
+#     print(f"write in : {directory}")
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#     with open(filename, "w", encoding='utf-8') as file:
+#         json.dump(clean_tweets, file)
 def write_file(clean_tweets, filename):
     print(f"clean-Write File : {filename}")
-    directory = os.path.dirname(filename)
+    directory = os.path.dirname(os.path.abspath(filename))
+    
     print(f"write in : {directory}")
     if not os.path.exists(directory):
         os.makedirs(directory)
     with open(filename, "w", encoding='utf-8') as file:
         json.dump(clean_tweets, file)
-
+        
 def generate_clean_tweets():
     print("... Limpiando tweets ...")
+    global diretion_new_dataset
+    diretion_new_dataset = "clean_data"
+    
     if not os.path.exists(diretion_new_dataset):
         os.makedirs(diretion_new_dataset)
 
@@ -43,20 +57,31 @@ def generate_clean_tweets():
     print("... Limpieza finalizada ...")
 
 
-def generate_clean_tweets_web(data):
+
+def generate_clean_tweets_web(json_files):
     print("... Limpiando tweets ...")
     global diretion_new_dataset
     diretion_new_dataset = "clean_data_dev"
-
+    output_file = os.path.join(diretion_new_dataset, "agregado.json")
     if not os.path.exists(diretion_new_dataset):
         os.makedirs(diretion_new_dataset)
-    clean_tweets = id_text(data)
-    write_file(clean_tweets, "documento")   
+
+    clean_tweets = id_text(json_files)
+    write_file(clean_tweets, output_file)
+    
+    
     print("... Limpieza finalizada ...")
 
-# def main():
-#     print("... Limpiando tweets ...")
-#     generate_clean_tweets()
-#     print("... Limpieza finalizada ...")
+# Parte 1
+generate_clean_tweets()
+
+
+# Parte 2
+""" direccion = 'dataset\\data_elecciones\\tweets_2018-08-07.json'
+with open(direccion, 'r' , encoding='utf-8') as f:
+    contenido_json = f.read()
+data = json.loads(contenido_json)
+#print(data)
+generate_clean_tweets_web(data) """
 
 
